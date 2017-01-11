01c9d520f8cc82001b1cf69fef394690b6da0de8
ms-lilibeth <ms.lilibeth@yandex.ru>
2017-01-11 22:47:10 +0500

    Non-tested AntColony added

--- Ant.py
+++ Ant.py
@@ -2,15 +2,25 @@ import random
 
 
 class Ant:
-    def __init__(self, graph, current_node):
+    def __init__(self, graph, current_node, destination):
         self._curr_node = current_node
         self._path_len = 0
         self._graph = graph
         self.pheromone_influence = 0.8
         # path influence = 1 - pheromone_influence
+        self._path = [current_node]
+        self._destination = destination
+        self._dead_end = False
 
     def move(self):
+        if self.is_stopped():
+            return
+
         adj_nodes = self._graph.get_adjacent_nodes(self._curr_node)
+        if len(adj_nodes) == 0:
+            self._dead_end = True
+            print("Warning: dead end! Ant stopped.")
+
         probabilities = {}
         denominator = self._graph.get_denominator(self.pheromone_influence)
         for n in adj_nodes:
@@ -28,5 +38,21 @@ class Ant:
                 move_to = i
                 break
         self._path_len += self._graph.get_weight(self._curr_node, move_to)
-        self._graph.update_pheromone(self._curr_node, move_to, self._path_len)
+        # self._graph.update_pheromone(self._curr_node, move_to, self._path_len)
         self._curr_node = move_to
+        self._path.append(move_to)
+
+    def get_current_node(self):
+        return self._curr_node
+
+    def get_path(self):
+        return self._path[:]
+
+    def get_path_length(self):
+        return self._path_len
+
+    def is_stopped(self):
+        return self._curr_node == self._destination or self._dead_end
+
+    def in_dead_end(self):
+        return self._dead_end
---
+++ AntColony.py
@@ -0,0 +1,36 @@
+from Ant import Ant
+
+
+class AntColony:
+    def __init__(self, size, graph, destination, start=1):
+        self._graph = graph
+        self._destination = destination
+        self._size = size
+        self._start = start
+
+    def find_best_path(self, iters_num):
+        paths = {}  # key - path length, value - list of paths of this length
+        for iteration in range(iters_num):
+            ants = [Ant(self._graph, self._start) for i in range(self._size)]
+            moving_ants = [a for a in ants]
+            while len(moving_ants) != 0:
+                for ant in moving_ants:
+                    ant.move()
+                moving_ants = [a for a in moving_ants if not a.is_stopped()]
+            finished_ants = [a for a in ants if a.is_stopped() and not a.in_dead_end()]
+            ants_in_dead_end = [a for a in ants if a.in_dead_end()]
+
+            for ant in finished_ants:
+                lgth = ant.get_path_length()
+                if lgth not in paths:
+                    paths[lgth] = []
+                p = ant.get_path()
+                if p not in paths[lgth]:
+                    paths[lgth].append(p)
+
+            # Print statistics
+            print("Iteration ", iteration, "*************")
+            print("    Ants in dead end: ", len(ants_in_dead_end))
+            print("    Best path length: ", min(paths))
+        min_lgth = min(paths)
+        return min_lgth, paths[min_lgth]
